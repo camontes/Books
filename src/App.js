0d347c85e36cd5f53292c27ef35ce52c1fe1aa3b
camontes <pope624@hotmail.com>
2025-04-14 12:16:48 -0500

    Create book

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import './index.css'
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from './components/BookList'
+import axios from 'axios';
 
 function App(){
 
     const[books, setBooks] = useState([]);
 
-    const createBook = (title) => {
+    const createBook = async (title) => {
         
-        const updatedBooks = [
-            ...books,
-            {
-                id: Math.round(Math.random() * 9999),
-                title
-            }
-        ];
+        const response = await axios.post('http://localhost:3001/books',{
+            title: title
+        });
 
+        const updatedBooks = [...books, response.data];
         setBooks(updatedBooks);
     };
 
